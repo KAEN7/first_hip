eb7124a99e8f22a2a595c6113055aabd8a2e6644
이성훈 <kusdsuna@naver.com>
2024-08-12 22:47:40 +0900

    feat: Analytics 활성화

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,32 @@
-import { Hydrate, QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import type { AppProps } from "next/app";
+import {
+	Hydrate,
+	QueryClient,
+	QueryClientProvider,
+} from "@tanstack/react-query";
 import { ThemeProvider } from "@emotion/react";
 import { RecoilRoot } from "recoil";
+import { Analytics } from "@vercel/analytics/react";
+import type { AppProps } from "next/app";
 import GlobalStyle from "@/styles/Global";
 import { theme } from "@/styles/theme";
 import Header from "@/components/ui/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
-    const queryClient = new QueryClient();
+	const queryClient = new QueryClient();
 
-    return (
-        <QueryClientProvider client={queryClient}>
-            <Hydrate state={pageProps.dehydrateState}>
-                <ThemeProvider theme={theme}>
-                    <RecoilRoot>
-                        <GlobalStyle />
-                        <Header />
-                        <Component {...pageProps} />
-                        {/* <Footer /> */}
-                        {/* <Analytics /> */}
-                    </RecoilRoot>
-                </ThemeProvider>
-            </Hydrate>
-        </QueryClientProvider>
-    );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Hydrate state={pageProps.dehydrateState}>
+				<ThemeProvider theme={theme}>
+					<RecoilRoot>
+						<GlobalStyle />
+						<Header />
+						<Component {...pageProps} />
+						{/* <Footer /> */}
+						<Analytics />
+					</RecoilRoot>
+				</ThemeProvider>
+			</Hydrate>
+		</QueryClientProvider>
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,76 +6,88 @@ import { useRouter } from "next/router";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    const keywords = [""].join(",");
-    const router = useRouter();
+	const keywords = [""].join(",");
+	const router = useRouter();
 
-    // TODO util로 분리
-    // TODO 이거는 지금 조회수가 없어서 넣어도 큰 의미가 없음
-    const getMarkupScheme = (
-        title?: string,
-        subTitle?: string,
-        url?: string,
-        thumbnailUrl?: string,
-        writer?: string,
-        publishedAt?: number,
-        updatedAt?: number
-    ) => {
-        function formatTimestamp(timestamp?: number): string {
-            const date = new Date(timestamp ? timestamp : Date.now());
-            let formattedDate = date.toISOString();
-            formattedDate = formattedDate.replace("Z", "+08:00");
+	// TODO util로 분리
+	// TODO 이거는 지금 조회수가 없어서 넣어도 큰 의미가 없음
+	const getMarkupScheme = (
+		title?: string,
+		subTitle?: string,
+		url?: string,
+		thumbnailUrl?: string,
+		writer?: string,
+		publishedAt?: number,
+		updatedAt?: number
+	) => {
+		function formatTimestamp(timestamp?: number): string {
+			const date = new Date(timestamp ? timestamp : Date.now());
+			let formattedDate = date.toISOString();
+			formattedDate = formattedDate.replace("Z", "+08:00");
 
-            return formattedDate;
-        }
+			return formattedDate;
+		}
 
-        return {
-            "@context": "https://schema.org",
-            "@type": "WebPage",
-            mainEntityOfPage: {
-                "@type": "WebPage",
-                "@id": `${process.env.NEXT_PUBLIC_DOMAIN}/${url}`, // URL
-            },
-            headline: `${title}`, // title
-            description: `${subTitle}`, // description
-            image: `vercel`, // thumbnail image
-            author: {
-                "@type": "Person",
-                name: `${writer}`, // author name
-            },
-            publisher: {
-                "@type": "Organization",
-                name: "kaen", // company name
-            },
-            datePublished: `${formatTimestamp(publishedAt)}`, // create date
-            dateModified: `${formatTimestamp(updatedAt)}`, // update date
-        };
-    };
+		return {
+			"@context": "https://schema.org",
+			"@type": "WebPage",
+			mainEntityOfPage: {
+				"@type": "WebPage",
+				"@id": `${process.env.NEXT_PUBLIC_DOMAIN}/${url}`, // URL
+			},
+			headline: `${title}`, // title
+			description: `${subTitle}`, // description
+			image: `vercel`, // thumbnail image
+			author: {
+				"@type": "Person",
+				name: `${writer}`, // author name
+			},
+			publisher: {
+				"@type": "Organization",
+				name: "kaen", // company name
+			},
+			datePublished: `${formatTimestamp(publishedAt)}`, // create date
+			dateModified: `${formatTimestamp(updatedAt)}`, // update date
+		};
+	};
 
-    useEffect(() => {
-        router.push("/descendant");
-    }, []);
+	useEffect(() => {
+		router.push("/descendant");
+	}, []);
 
-    return (
-        <>
-            {/* TODO 나중에 컴포넌트로 분리 */}
-            <Head>
-                <title>퍼덩이</title>
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content={`${process.env.NEXT_PUBLIC_DOMAIN}`} />
-                <meta property="og:title" content="퍼덩이" />
-                <meta property="og:image" content="/images/logo.png" />
-                <meta property="og:image:width" content="800" />
-                <meta property="og:image:height" content="420" />
-                <meta property="og:description" content="퍼스트 디센던트를 즐기기 위한 다양한 정보와 공략을 알아보세요" />
-                <meta property="og:site_name" content="퍼덩이" />
-                <meta name="description" content="퍼스트 디센던트를 즐기기 위한 다양한 정보와 공략을 알아보세요" />
-                <meta name="twitter:title" content="퍼덩이" />
-                <meta name="twitter:description" content="퍼스트 디센던트를 즐기기 위한 다양한 정보와 공략을 알아보세요" />
-                <meta name="keywords" content={keywords} />
-                <meta name="twitter:title" content="퍼덩이" />
-                <meta name="twitter:description" content="퍼스트 디센던트를 즐기기 위한 다양한 정보와 공략을 알아보세요" />
-                {/* <meta name="google-site-verification" content="구글서치콘솔키" /> */}
-                {/* <script
+	return (
+		<>
+			{/* TODO 나중에 컴포넌트로 분리 */}
+			<Head>
+				<title>퍼덩이</title>
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content={`${process.env.NEXT_PUBLIC_DOMAIN}`} />
+				<meta property="og:title" content="퍼덩이" />
+				<meta property="og:image" content="/images/logo.png" />
+				<meta property="og:image:width" content="800" />
+				<meta property="og:image:height" content="420" />
+				<meta
+					property="og:description"
+					content="퍼스트 디센던트를 즐기기 위한 다양한 정보와 공략을 알아보세요"
+				/>
+				<meta property="og:site_name" content="퍼덩이" />
+				<meta
+					name="description"
+					content="퍼스트 디센던트를 즐기기 위한 다양한 정보와 공략을 알아보세요"
+				/>
+				<meta name="twitter:title" content="퍼덩이" />
+				<meta
+					name="twitter:description"
+					content="퍼스트 디센던트를 즐기기 위한 다양한 정보와 공략을 알아보세요"
+				/>
+				<meta name="keywords" content={keywords} />
+				<meta name="twitter:title" content="퍼덩이" />
+				<meta
+					name="twitter:description"
+					content="퍼스트 디센던트를 즐기기 위한 다양한 정보와 공략을 알아보세요"
+				/>
+				{/* <meta name="google-site-verification" content="구글서치콘솔키" /> */}
+				{/* <script
                     type="application/ld+json"
                     dangerouslySetInnerHTML={{
                         __html: JSON.stringify(
@@ -83,10 +95,10 @@ export default function Home() {
                         ),
                     }}
                 /> */}
-                <link rel="canonical" href={`${process.env.NEXT_PUBLIC_DOMAIN}`} />
-            </Head>
+				<link rel="canonical" href={`${process.env.NEXT_PUBLIC_DOMAIN}`} />
+			</Head>
 
-            {/* <main className={`${styles.main} ${inter.className}`}></main> */}
-        </>
-    );
+			{/* <main className={`${styles.main} ${inter.className}`}></main> */}
+		</>
+	);
 }
